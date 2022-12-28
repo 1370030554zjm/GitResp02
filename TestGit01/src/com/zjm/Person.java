154514bc71582d7a1c476997ae5e8dc7102336e6
郑佳明 <zjm@126.com>
2022-12-28 12:01:07 +0800

    新增了Person类，添加了age属性...

---
+++ TestGit01/src/com/zjm/Person.java
@@ -0,0 +1,13 @@
+package com.zjm;
+
+/**
+ * @Auther: 郑佳明
+ * @Date: 2022/12/28 - 12 - 28 - 11:59
+ * @Description: com.zjm
+ * @version: 1.0
+ */
+public class Person {
+
+    private int age;
+
+}
