e1a7b4ecb412370ed551ad3a23d6d994934f08a7
郑佳明 <zjm@126.com>
2022-12-28 10:40:27 +0800

    新增了构造方法，get与set方法

--- TestGit01/src/com/zjm/Student.java
+++ TestGit01/src/com/zjm/Student.java
@@ -1,14 +1,57 @@
 package com.zjm;
 
+import java.io.Serializable;
+
 /**
  * @Auther: 郑佳明
  * @Date: 2022/12/27 - 12 - 27 - 12:51
  * @Description: com.zjm
  * @version: 1.0
  */
-public class Student {
+public class Student implements Serializable {
     private String name;
     private int age;
     private String gender;
 
+    public Student(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
